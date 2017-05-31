ef643e94c99a06eb70f05f4a24c860a9e1579ae3
Rhio Kim <rhio.kim@gmail.com>
2017-05-31 19:50:03 +0900

    ci: add gh-pages script (wip)

--- libs/md-loader.js
+++ libs/md-loader.js
@@ -3,12 +3,12 @@ import remark from "remark";
 import html from "remark-html";
 import hljs from "remark-highlight.js";
 import slug from "remark-slug";
-import heading from "remark-autolink-headings";
+import headding from "remark-autolink-headings";
 // import recommended from "remark-presset-lint-recommended";
 
 module.exports = file => {
   let markdown = fs.readFileSync(file).toString("utf8");
-  let vfile = remark().use([slug, heading, hljs, html]).processSync(markdown);
+  let vfile = remark().use([slug, headding, hljs, html]).processSync(markdown);
 
   return vfile.contents;
 };
