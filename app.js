c59d6332971ebcc8420bab98cb7c74f03138f6f4
Rhio Kim <rhio.kim@gmail.com>
2017-05-31 14:04:03 +0900

    add static files and improve main template

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import React, { createElement } from "react";
 import { renderToString, renderToStaticMarkup } from "react-dom/server";
-import Router from "react-router";
+import fs from "fs";
+import copy from "recursive-copy";
 
 import template from "./template";
 
@@ -9,4 +10,10 @@ const App = require("./pages/App");
 const html = template({
   title: "react-book",
   body: renderToStaticMarkup(createElement(App, { data: [] }))
+});
+
+fs.writeFileSync("./out/index.html", html, { encoding: "utf8" });
+
+copy("static", "out/static").then(result => {
+  console.log(result);
 });
--- template.js
+++ template.js
@@ -1,17 +1,30 @@
+const defaultDescription = "";
+const defaultOGURL = "";
+const defaultOGImage = "";
+
 module.exports = ({ body, title }) => {
   return `
     <!DOCTYPE html>
     <html>
-      <head>
-        <title>${title}</title>
-        <link rel="stylesheet" href="/assets/index.css" />
-      </head>
+      <meta charset="UTF-8" />
+      <title>${title}</title>
+      <meta
+        name="description"
+        content=${defaultDescription}
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      <link
+        href="//fonts.googleapis.com/css?family=Rubik|Unica+One"
+        rel="stylesheet"
+      />
+      <link rel="stylesheet" href="/static/css/normalize.css" />
+      <link rel="stylesheet" href="/static/css/flexboxgrid.min.css" />
+      <link rel="stylesheet" href="/static/css/main.css" />
 
       <body>
         <div id="root">${body}</div>
       </body>
-
-      <script src="/assets/bundle.js"></script>
     </html>
   `;
 };
